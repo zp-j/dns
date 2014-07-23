27fbf368605ed82a20456211c4a3eff70850bdf8
Miek Gieben <miek@miek.nl>
2014-07-23 07:58:22 +0100

    add some documentation

--- sig0.go
+++ sig0.go
@@ -4,15 +4,21 @@
 
 // SIG(0)
 //
-// blah
+// From RFC 2931:
+//
+//     SIG(0) provides protection for DNS transactions and requests ....
+//     ... protection for glue records, DNS requests, protection for message headers 
+//     on requests and responses, and protection of the overall integrity of a response.
+//
+//
 package dns
 
 // SIG0Sign will sign the ...
-func (dns *Msg) SIG0Sign() {
-
+func (dns *Msg) SIG0Sign() ([]byte, error) {
+	return nil, nil
 }
 
 // SIG0Verify will bla bla
-func SIG0Verify([]byte) {
-
+func SIG0Verify([]byte) bool {
+	return false
 }
